f46febadafb4dbce9700cae42a7cd59e66ef0ad5
DimaKovalencko <71615244+DimaKovalencko@users.noreply.github.com>
2020-09-21 08:43:06 +0300

    Add files via upload

---
+++ to2rent/JS/main.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.slider').slick({
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="img/prev.svg" alt="prev-slaid"></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="img/next.svg" alt="next-slaid"></button>',
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        
+
+
+    })
+
+})
